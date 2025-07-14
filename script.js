cad9ecfbfc2120b66783615bad77d2b5b10e67ad
Tasnimmou11 <ummemou44@gmail.com>
2025-07-14 23:27:36 +0600

    operator

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ let population = 10;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+//operator
+const ageNow = 2037;
+const ageJonas = ageNow - 1991;
+const ageSara = ageNow - 2018;
+console.log(ageJonas, ageSara);
+console.log("age " + ageJonas * 2, ageSara / 2, 2 ** 3);
+//3**2=2*2*2;
+
+let x = 10 + 5; //15
+x += 10; //here x=x+10
+console.log(x);
