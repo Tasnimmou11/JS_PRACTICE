d555389837be921d8fe904c314d24adec34deb44
Tasnimmou11 <ummemou44@gmail.com>
2025-07-16 23:32:37 +0600

    operator

--- script.js
+++ script.js
@@ -20,3 +20,15 @@ console.log("age " + ageJonas * 2, ageSara / 2, 2 ** 3);
 let x = 10 + 5; //15
 x += 10; //here x=x+10
 console.log(x);
+
+//asignment
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 95;
+const heightJohn = 1.88;
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log("BMIMark" + " " + BMIMark);
+console.log("BMIJohn" + " " + BMIJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
